7d72b0d9a0c9186fe4bd7d2f31389215bbdc17c8
Erfan <143953545+bigerfan@users.noreply.github.com>
2026-08-24 13:24:47 +0330

    remove updateUsd cron

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ import cors from "cors";
 import path from "path";
 import bodyParser from "body-parser";
 import "./cron/verifyPaymenCron";
-import "./cron/updateUsdToIrr";
+// import "./cron/updateUsdToIrr";
 import { errorHandler } from "./middleware/errorHanderMiddleware";
 // import "./cron/dailyDiscount";
 
